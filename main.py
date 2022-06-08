8e7eb285e7017895e8022644d704671e57410467
eric bel <ericbelecov@gmail.com>
2022-06-08 23:44:08 +0400

    telebot installed

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from gettext import install
-from lib2to3.pgen2 import token
-from turtle import update
-import json
+import telebot
 import const
-import time
-import requests
-from urllib import request, response
-import const
-from pprint import pprint
-
-def main():
-    while True:
-        url = const.URL.format(TOKEN=const.TOKEN, method=const.UPDATE_METH)
-        content = requests.get(url).text
-        data = json.loads(content)
-        pprint(data)
-        time.sleep(2)
 
 
-if __name__ == "__main__":
-    main()
+token = const.TOKEN
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Привет ✌️, дотсупные команды: \n/dan, \n/lily")
+
+
+@bot.message_handler(commands=['dan'])
+def start_message(message):
+    bot.send_message(
+    message.chat.id, "Привет ✌️, а кто такой Даник?, это король режима Metro!")
+
+@bot.message_handler(commands=['lily'])
+def start_message(message):
+    bot.send_message(
+    message.chat.id, "Привет ✌️, а кто такой Лилия?, это самая обаятельная девушка в мире!")
+
+
+bot.infinity_polling()
